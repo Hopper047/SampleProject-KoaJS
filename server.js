c82c227e77dd929cfcf22be550296df26d0bb2bf
Amoharir <adityamoharir007@gmail.com>
2024-06-03 14:51:43 +0530

    koa: simple request & response

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-const koa=require('koa')
-const app=new koa()
+const koa = require("koa");
+const app = new koa();
 
 // add a date method to the content
-app.context.date = Date()
+app.context.date = Date();
+app.context.useData = { firstName: "John", LastName: "Wick" };
 
-app.use(ctx => {
-    // use the state
-    ctx.state.user = "Hopper"
+app.use((ctx) => {
+  // use the state
+  ctx.state.user = "Hopper";
+  //request object usage
+  const from = ctx.request.origin;
+  console.log("Origin: ", from);
+  const method = ctx.request.method;
+  console.log("Method: ", method);
 
-    ctx.body = `Hello ${ctx.state.user} to KOA.js, today is ${ctx.date}`
+  ctx.body = `Hello ${ctx.state.user} to KOA.js, today is ${ctx.date}`;
 
-    /* 
+  //response object usage
+  ctx.response.body = `This response is coming from ${from} Origin, with ${method} method`;
+  if (ctx.useData && ctx.useData.firstName == "John")
+    ctx.response.body = ctx.useData;
+  else ctx.throw(404, "Data not found");
+
+  /* 
     ctx.state: state similar to React, can send to FE
     ctx.cookies: manage cookies
     ctx.throw: throw an Exception 
     ctx.body: Print to browser
 
-    Documentation: https://Koajs.com/#context
+    Documentation: 
+    https://Koajs.com/#context
+    https://Koajs.com/#request
+    https://Koajs.com/#response
 
     */
-})
+});
 
-app.listen(3000) //http://localhost:3000/
+app.listen(3000); //http://localhost:3000/
