79abe3da5933298034886bccfeec63b7912781a0
Amoharir <adityamoharir007@gmail.com>
2024-06-03 12:49:20 +0530

    koa: context method & state

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const koa=require('koa')
 const app=new koa()
 
-app.use(ele => {
-    ele.body = "Hello to KOA.js"
+// add a date method to the content
+app.context.date = Date()
+
+app.use(ctx => {
+    // use the state
+    ctx.state.user = "Hopper"
+
+    ctx.body = `Hello ${ctx.state.user} to KOA.js, today is ${ctx.date}`
+
+    /* 
+    ctx.state: state similar to React, can send to FE
+    ctx.cookies: manage cookies
+    ctx.throw: throw an Exception 
+    ctx.body: Print to browser
+
+    Documentation: https://Koajs.com/#context
+
+    */
 })
 
 app.listen(3000) //http://localhost:3000/
